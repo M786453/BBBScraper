1b360202fb5f9234440cf74f7efaca879c161d3d
Ahtesham Sarwar <57073399+M786453@users.noreply.github.com>
2024-05-09 00:19:09 +0500

    storing in excel done

--- BBB_Data_Scrapper.py
+++ BBB_Data_Scrapper.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from urllib.parse import quote
+from openpyxl import Workbook
 import time
 
 country, location, txt = "USA", "California", "it services"
 
 page = 1
+
+all_businesses_data = []
 
 encoded_location = quote(location)
 
@@ -66,7 +68,9 @@ for pageNumber in range(1, total_Pages+1):
         
             try:
 
-                business_data_dict = {"Overview": '', "Product & Services": '',"Business Started": '',  "Hours of Operation" : '', "Contact Information" : '', "Business Categories" : '', 'Website': '', "Phone Number":''}
+                business_data_dict = {"Title": '',"Overview": '', "Product & Services": '',"Business Started": '',  "Hours of Operation" : '', "Contact Information" : '', "Business Categories" : '', 'Website': '', "Phone Number":''}
+
+                business_data_dict["Title"] = link.text
 
                 driver.switch_to.window(driver.window_handles[1]) #switch to second tab
                 
@@ -138,12 +142,28 @@ for pageNumber in range(1, total_Pages+1):
 
                     print("Error @ contact info")
 
-                print(business_data_dict)
+                all_businesses_data.append(business_data_dict)
 
                 time.sleep(10)
       
             except:
                 print("Skipping:", link_attrb)
 
-# https://www.bbb.org/us/or/portland/profile/esthetician/oregon-laser-wellness-center-1296-1000047797/details
-# https://www.bbb.org/us/or/portland/profile/esthetician/oregon-laser-wellness-center-1296-1000047797
+# Store data in excel
+
+wb = Workbook()
+
+ws = wb.active
+
+if len(all_businesses_data) > 0:
+
+    headers_list = all_businesses_data[0].keys()
+
+    ws.append(list(headers_list))
+
+    for business in all_businesses_data:
+
+        ws.append(list(business.values()))
+
+
+wb.save('Output.xlsx')
