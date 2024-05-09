00e1e4db86c0351a30ada3c6aca8725fd7f334c9
Ahtesham Sarwar <57073399+M786453@users.noreply.github.com>
2024-05-10 00:36:31 +0500

    BBB scraper updated

--- BBB_Data_Scrapper.py
+++ BBB_Data_Scrapper.py
@@ -1,8 +1,48 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from urllib.parse import quote
 from openpyxl import Workbook
+from openpyxl import load_workbook
 import time
+import json
+import os
+
+def update_progress(progress_data):
+
+    with open('progress.json', 'w') as f:
+
+        f.write(json.dumps(progress_data))
+
+def load_progress_tracker():
+
+    progress_status, progress = 'New', {}
+
+    if os.path.exists('progress.json'):
+
+        user_input = input("Do you want to continue old scraping process?\nEnter (Yes/No): ")
+
+        if user_input in ['Yes', 'No']:
+            
+            if 'Yes' == user_input:
+
+                try:
+
+                    with open('progress.json', 'r') as f:
+
+                        progress = json.loads(f.read())
+
+                    progress_status = "Old"
+                    
+                except Exception as e:
+
+                    print("Error:", e)    
+    
+        else:
+
+            print("Invalid Input.")
+            
+            progress_status = "Stop"
+    
+    return progress_status, progress
 
 def get_user_input():
 
@@ -40,6 +80,8 @@ def get_business_details(driver, link, link_attrb):
 
     business_data_dict = {"Title": '',"Overview": '', "Product & Services": '',"Business Started": '',  "Hours of Operation" : '', "Contact Information" : '', "Business Categories" : '', 'Website': '', "Phone Number":''}
 
+    is_all_extracted = False
+
     try:
         
         business_data_dict["Title"] = link.text
@@ -62,7 +104,7 @@ def get_business_details(driver, link, link_attrb):
                 business_data_dict["Overview"] = overview
 
         except:
-            
+
             print("Error @ Overview")
 
         try:
@@ -78,26 +120,31 @@ def get_business_details(driver, link, link_attrb):
 
         # Business Started, Hours of Operation, All contact person, Business categories
 
-        div =  driver.find_element(By.XPATH, "//div[@class='stack css-n8vred e1ri33r70']")
+        try:
+            div =  driver.find_element(By.XPATH, "//div[@class='stack css-n8vred e1ri33r70']")
 
-        data_heading = div.find_elements(By. TAG_NAME, "dt" )
+            data_heading = div.find_elements(By. TAG_NAME, "dt" )
 
-        data_value = div.find_elements(By. TAG_NAME, "dd")
+            data_value = div.find_elements(By. TAG_NAME, "dd")
 
-        for index in range(len(data_heading)):
+            for index in range(len(data_heading)):
 
-            try:
-                label = data_heading[index].text
-                if label in required_data_list:
-                    if label.endswith(":"):
-                        label = label[:-1]
+                try:
+                    label = data_heading[index].text
+                    if label in required_data_list:
+                        if label.endswith(":"):
+                            label = label[:-1]
 
-                    value = data_value[index].text
-                    
-                    business_data_dict[label] = value
-            except:
+                        value = data_value[index].text
+                        
+                        business_data_dict[label] = value
+                except:
 
-                print("Error in heading loop")
+                    print("Error in heading loop")
+        
+        except Exception as e:
+
+            print("Error:", e)
 
         # Contact Info
         ## Phone Number, Website
@@ -117,56 +164,107 @@ def get_business_details(driver, link, link_attrb):
             except:
                 print("Error @ Phone Number")
 
+            is_all_extracted = True
+
         except:
 
             print("Error @ contact info")
 
-        return business_data_dict
         
     except:
         
         print("Skipping:", link_attrb)
 
-    return business_data_dict
+    return is_all_extracted, business_data_dict
 
 
 if __name__ == "__main__":
 
-    wb = Workbook() # Workbook
+    driver = webdriver.Chrome()
 
-    ws = wb.active # Worksheet
+    progress_status, progress = load_progress_tracker()
 
-    headers = ["Title", "Overview", "Product & Services", "Business Started",  "Hours of Operation", "Contact Information", "Business Categories", 'Website', "Phone Number"]
+    wb = None
 
-    ws.append(headers) # Write headers in Worksheet
+    ws = None
 
-    wb.save('Output.xlsx') # Save Excel File
-
-    query, location, country = get_user_input() # Get User Input
-
-    encoded_location = quote(location)
-
-    encoded_txt = quote(query)
+    query, location, country = '', '', ''
 
     required_data_list = ["Business Started:", "Hours of Operation", "Contact Information", "Business Categories"]
 
+    total_pages = 0
+
+    # Setup
+
+    if progress_status == "Stop":
+
+        exit()
+    
+    elif progress_status == "New":
+
+        wb = Workbook() # Workbook
+
+        ws = wb.active # Worksheet
+
+        progress = {'query': '', 'location': '', 'country': '', 'page_no': 1, 'total_pages' : '', 'visited_businesses': []}
+
+        headers = ["Title", "Overview", "Product & Services", "Business Started",  "Hours of Operation", "Contact Information", "Business Categories", 'Website', "Phone Number"]
+
+        ws.append(headers) # Write headers in Worksheet
+
+        wb.save('Output.xlsx') # Save Excel File
+
+        query, location, country = get_user_input() # Get User Input
+
+        progress['query'] = query
+
+        progress['location'] = location
+        
+        progress['country'] = country
+
+    elif progress_status == "Old":
+
+        wb = load_workbook('Output.xlsx')
+
+        ws = wb.active # Worksheet
+
+        query = progress['query']
+
+        location = progress['location']
+
+        country = progress['country']
+
+    # Scraping Process
+
     url = f"https://www.bbb.org/search?find_country={country}&find_loc={location}&find_text={query}&page="
 
-    driver = webdriver.Chrome()
+    if progress_status == "New":
+    
+        driver.get(url+"1")
 
-    driver.get(url+"1")
+        time.sleep(10)
 
-    time.sleep(10)
+        total_pages = get_total_pages(driver)
 
-    total_pages = get_total_pages(driver)
+        progress["total_pages"] = total_pages
+    
+    elif progress_status == "Old":
 
-    driver.execute_script(f'''window.open("","_blank");''')
+        total_pages = progress["total_pages"]
 
-    print("PAGES:", total_pages)
+    driver.execute_script(f'''window.open("","_blank");''') # Open new tab
+
+    print("Total PAGES:", total_pages)
 
     for pageNumber in range(1, total_pages+1):
+        
+        if pageNumber < progress["page_no"]:
+
+            continue
 
         print("PAGE#", pageNumber)
+
+        progress["page_no"] = pageNumber
 
         driver.switch_to.window(driver.window_handles[0]) #switch to first tab
         
@@ -187,10 +285,26 @@ if __name__ == "__main__":
             
             link_attrb = link.get_attribute("href")
             
-            if link_attrb.startswith("https://www.bbb.org/"):
-            
-                business = get_business_details(driver, link, link_attrb)
+            if link_attrb.startswith("https://www.bbb.org/") and link_attrb not in progress['visited_businesses']:
 
-                ws.append(list(business.values()))
+                is_all_extracted, business = get_business_details(driver, link, link_attrb)
 
-                wb.save('Output.xlsx')
+                if is_all_extracted:
+
+                    progress['visited_businesses'].append(link_attrb)
+
+                    ws.append(list(business.values()))
+
+                    wb.save('Output.xlsx')
+
+                    update_progress(progress)
+        
+        progress['visited_businesses'] = []
+    
+
+    # Remove progress file if scraping process done
+
+    try:
+        os.remove('progress.json')
+    except Exception as e:
+        print("Error:", e)
