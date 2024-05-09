f43e701a88566a7fede739421dfb47d605e1b9b7
Ahtesham Sarwar <57073399+M786453@users.noreply.github.com>
2024-05-09 21:04:09 +0500

    get_user_input function added

--- BBB_Data_Scrapper.py
+++ BBB_Data_Scrapper.py
@@ -4,174 +4,183 @@ from urllib.parse import quote
 from openpyxl import Workbook
 import time
 
-txt = input("Enter Your Query: ")
+def get_user_input():
 
-location = input("Enter Location: ")
+    query = input("Enter Your Query: ")
 
-country = input("Enter Country (USA/CA): ")
+    location = input("Enter Location: ")
 
-if country not in ["USA", "CA"]:
-    print("Invalid Country.")
-    exit()
+    country = input("Enter Country (USA/CA): ")
 
-page = 1
+    if country not in ["USA", "CA"]:
+        print("Invalid Country.")
+        exit()
 
-all_businesses_data = []
+    return [query, location, country]
 
-encoded_location = quote(location)
 
-encoded_txt = quote(txt)
+if __name__ == "__main__":
 
-required_data_list = ["Business Started:", "Hours of Operation", "Contact Information", "Business Categories"]
+    query, location, country = get_user_input()
 
-url = f"https://www.bbb.org/search?find_country={country}&find_loc={location}&find_text={txt}&page="
+    page = 1
 
-driver = webdriver.Chrome()
+    all_businesses_data = []
 
-driver.get(url+"1")
+    encoded_location = quote(location)
 
-time.sleep(10)
+    encoded_txt = quote(query)
 
-business_links = driver.find_elements(By.XPATH, "//a[@class='text-blue-medium css-1jw2l11 eou9tt70']")
+    required_data_list = ["Business Started:", "Hours of Operation", "Contact Information", "Business Categories"]
 
-try:
-    
-    paging = driver.find_element(By.XPATH, "//nav[@aria-label='pagination']")
+    url = f"https://www.bbb.org/search?find_country={country}&find_loc={location}&find_text={query}&page="
 
-    anchor = paging.find_elements(By.TAG_NAME, "a")[-3]
+    driver = webdriver.Chrome()
 
-    total_Pages = int(anchor.text.strip(" ")[-1])
+    driver.get(url+"1")
 
-except Exception as e:
-    print("Error:", e)
-    total_Pages = 1
-
-driver.execute_script(f'''window.open("","_blank");''')
-
-print("PAGES:", total_Pages)
-
-for pageNumber in range(1, total_Pages+1):
-
-    print("PAGE#", pageNumber)
-
-    driver.switch_to.window(driver.window_handles[0]) #switch to first tab
-    
-    while True:
-        driver.get(url + str(pageNumber))
-        if "500 Error" not in driver.page_source:
-            break
-        time.sleep(10)
+    time.sleep(10)
 
     business_links = driver.find_elements(By.XPATH, "//a[@class='text-blue-medium css-1jw2l11 eou9tt70']")
-    
-    time.sleep(20) #testing
 
-    for link in business_links:
+    try:
+        
+        paging = driver.find_element(By.XPATH, "//nav[@aria-label='pagination']")
+
+        anchor = paging.find_elements(By.TAG_NAME, "a")[-3]
+
+        total_Pages = int(anchor.text.strip(" ")[-1])
+
+    except Exception as e:
+        print("Error:", e)
+        total_Pages = 1
+
+    driver.execute_script(f'''window.open("","_blank");''')
+
+    print("PAGES:", total_Pages)
+
+    for pageNumber in range(1, total_Pages+1):
+
+        print("PAGE#", pageNumber)
 
         driver.switch_to.window(driver.window_handles[0]) #switch to first tab
         
-        link_attrb = link.get_attribute("href")
+        while True:
+            driver.get(url + str(pageNumber))
+            if "500 Error" not in driver.page_source:
+                break
+            time.sleep(10)
+
+        business_links = driver.find_elements(By.XPATH, "//a[@class='text-blue-medium css-1jw2l11 eou9tt70']")
         
-        if link_attrb.startswith("https://www.bbb.org/"):
+        time.sleep(20) #testing
+
+        for link in business_links:
+
+            driver.switch_to.window(driver.window_handles[0]) #switch to first tab
+            
+            link_attrb = link.get_attribute("href")
+            
+            if link_attrb.startswith("https://www.bbb.org/"):
+            
+                try:
+
+                    business_data_dict = {"Title": '',"Overview": '', "Product & Services": '',"Business Started": '',  "Hours of Operation" : '', "Contact Information" : '', "Business Categories" : '', 'Website': '', "Phone Number":''}
+
+                    business_data_dict["Title"] = link.text
+
+                    driver.switch_to.window(driver.window_handles[1]) #switch to second tab
+                    
+                    # Overview and Product & Services
+
+                    driver.get(link_attrb)
+
+                    time.sleep(10)
+
+                    try:
+                        overview = driver.find_element(By.XPATH, "//div[contains(@class,'dtm-overview')]//div").text
+                        business_data_dict["Overview"] = overview
+                    except:
+                        print("Error @ Overview")
+
+                    try:
+                        products_services = driver.find_element(By.XPATH, "//div[contains(@class,'dtm-products-services')]").text
+                        business_data_dict["Product & Services"] = products_services
+                    except:
+                        print("Error @ Products & Services")
+
+
+                    driver.get(link_attrb + "/details")
+
+                    time.sleep(10)
+
+                    # Business Started, Hours of Operation, All contact person, Business categories
+
+                    div =  driver.find_element(By.XPATH, "//div[@class='stack css-n8vred e1ri33r70']")
+
+                    data_heading = div.find_elements(By. TAG_NAME, "dt" )
+
+                    data_value = div.find_elements(By. TAG_NAME, "dd")
+
+                    for index in range(len(data_heading)):
+
+                        try:
+                            label = data_heading[index].text
+                            if label in required_data_list:
+                                if label.endswith(":"):
+                                    label = label[:-1]
+
+                                value = data_value[index].text
+                                
+                                business_data_dict[label] = value
+                        except:
+
+                            print("Error in heading loop")
+
+                    # Contact Info
+                    ## Phone Number, Website
+
+                    try:
+                        contact_div = driver.find_element(By.XPATH, "//div[contains(@class,'dtm-contact')]")
+
+                        try:
+                            website = contact_div.find_element(By.TAG_NAME, "a")
+                            business_data_dict["Website"] = website.get_attribute('href')
+                        except:
+                            print("Error @ website")
+
+                        try:
+                            phone_number = contact_div.find_element(By.XPATH, "//a[contains(@class,'dtm-phone')]")
+                            business_data_dict["Phone Number"] = phone_number.text
+                        except:
+                            print("Error @ Phone Number")
+
+                    except:
+
+                        print("Error @ contact info")
+
+                    all_businesses_data.append(business_data_dict)
+
+                    time.sleep(10)
         
-            try:
-
-                business_data_dict = {"Title": '',"Overview": '', "Product & Services": '',"Business Started": '',  "Hours of Operation" : '', "Contact Information" : '', "Business Categories" : '', 'Website': '', "Phone Number":''}
-
-                business_data_dict["Title"] = link.text
-
-                driver.switch_to.window(driver.window_handles[1]) #switch to second tab
-                
-                # Overview and Product & Services
-
-                driver.get(link_attrb)
-
-                time.sleep(10)
-
-                try:
-                    overview = driver.find_element(By.XPATH, "//div[contains(@class,'dtm-overview')]//div").text
-                    business_data_dict["Overview"] = overview
                 except:
-                    print("Error @ Overview")
+                    print("Skipping:", link_attrb)
 
-                try:
-                    products_services = driver.find_element(By.XPATH, "//div[contains(@class,'dtm-products-services')]").text
-                    business_data_dict["Product & Services"] = products_services
-                except:
-                    print("Error @ Products & Services")
+    # Store data in excel
 
+    wb = Workbook()
 
-                driver.get(link_attrb + "/details")
+    ws = wb.active
 
-                time.sleep(10)
+    if len(all_businesses_data) > 0:
 
-                # Business Started, Hours of Operation, All contact person, Business categories
+        headers_list = all_businesses_data[0].keys()
 
-                div =  driver.find_element(By.XPATH, "//div[@class='stack css-n8vred e1ri33r70']")
+        ws.append(list(headers_list))
 
-                data_heading = div.find_elements(By. TAG_NAME, "dt" )
+        for business in all_businesses_data:
 
-                data_value = div.find_elements(By. TAG_NAME, "dd")
-
-                for index in range(len(data_heading)):
-
-                    try:
-                        label = data_heading[index].text
-                        if label in required_data_list:
-                            if label.endswith(":"):
-                                label = label[:-1]
-
-                            value = data_value[index].text
-                            
-                            business_data_dict[label] = value
-                    except:
-
-                        print("Error in heading loop")
-
-                # Contact Info
-                ## Phone Number, Website
-
-                try:
-                    contact_div = driver.find_element(By.XPATH, "//div[contains(@class,'dtm-contact')]")
-
-                    try:
-                        website = contact_div.find_element(By.TAG_NAME, "a")
-                        business_data_dict["Website"] = website.get_attribute('href')
-                    except:
-                        print("Error @ website")
-
-                    try:
-                        phone_number = contact_div.find_element(By.XPATH, "//a[contains(@class,'dtm-phone')]")
-                        business_data_dict["Phone Number"] = phone_number.text
-                    except:
-                        print("Error @ Phone Number")
-
-                except:
-
-                    print("Error @ contact info")
-
-                all_businesses_data.append(business_data_dict)
-
-                time.sleep(10)
-      
-            except:
-                print("Skipping:", link_attrb)
-
-# Store data in excel
-
-wb = Workbook()
-
-ws = wb.active
-
-if len(all_businesses_data) > 0:
-
-    headers_list = all_businesses_data[0].keys()
-
-    ws.append(list(headers_list))
-
-    for business in all_businesses_data:
-
-        ws.append(list(business.values()))
+            ws.append(list(business.values()))
 
 
-wb.save('Output.xlsx')
+    wb.save('Output.xlsx')
